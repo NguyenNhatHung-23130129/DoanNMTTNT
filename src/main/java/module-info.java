89d74ba1decf3532483a9f36c8e55b74edc389a9
NguyenNhatHung-23130129 <23130129@st.hcmuaf.edu.vn>
2025-11-12 19:20:47 +0700

    firt commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module vn.edu.hcmuaf.fit.doannmttnt {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens vn.edu.hcmuaf.fit.doannmttnt to javafx.fxml;
+    exports vn.edu.hcmuaf.fit.doannmttnt;
+}
