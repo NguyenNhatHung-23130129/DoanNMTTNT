0b6d767ef0c4ac07fb90c643f87751fffebe01ea
NguyenNhatHung-23130129 <23130129@st.hcmuaf.edu.vn>
2025-12-08 14:14:12 +0700

    tai cau truc cac lop

--- src/main/java/othello/GameEndStrategy.java
+++
@@ -1,8 +0,0 @@
-package othello;
-
-public class GameEndStrategy {
-    public boolean checkWin(Board board, Player player, int winPiece) {
-        // Implement win checking logic here
-        return false;
-    }
-}
